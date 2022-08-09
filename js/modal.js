ad72dc971aad7c73c28dfb5ff00fd1ccb6672a08
Serhei Honcharov <mrsedmor@gmail.com>
2022-08-09 21:32:25 +0200

    Адаптивная верстка index.html

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,27 @@
 (() => {
-	const refs = {
-		openModalBtn: document.querySelector("[data-modal-open]"),
-		closeModalBtn: document.querySelector("[data-modal-close]"),
-		modal: document.querySelector("[data-modal]"),
-	};
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    modalParent: document.querySelector('[data-modal-parent'),
+    menu: document.querySelector('[data-menu'),
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+  };
 
-	refs.openModalBtn.addEventListener("click", toggleModal);
-	refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-	function toggleModal() {
-		refs.modal.classList.toggle("is-hidden");
-	}
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    refs.modalParent.classList.toggle('block-scroll');
+  }
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('page-menu--hidden');
+    // refs.modalParent.classList.toggle('block-scroll');
+  }
 })();
