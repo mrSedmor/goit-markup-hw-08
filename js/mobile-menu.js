0b701912c9b5dcd7e2cffd5114d0d4dbc7a691f0
Serhei Honcharov <mrsedmor@gmail.com>
2022-08-12 00:02:41 +0200

    Незначительные правки
    Отредактировал стили, убрал лишнее.
    Добавил aria-controls, aria-expanded в меню страницы.
    Добавил прокрутку в модальное окто и меню страницы если не влазит на экран.
    Добавил анимацию появления меню страницы.

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,13 +3,30 @@
     menu: document.querySelector('[data-menu'),
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
+    modalParent: document.querySelector('[data-modal-parent'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
   function toggleMenu() {
+    const expanded = refs.openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    refs.openMenuBtn.setAttribute('aria-expanded', !expanded);
     refs.menu.classList.toggle('page-menu--hidden');
-    // refs.modalParent.classList.toggle('block-scroll');
+    refs.modalParent.classList.toggle('block-scroll--mobile');
   }
 })();
+
+// (() => {
+//   const menuBtnRef = document.querySelector('[data-menu-button]');
+//   const mobileMenuRef = document.querySelector('[data-menu]');
+
+//   menuBtnRef.addEventListener('click', () => {
+//     const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+//     menuBtnRef.classList.toggle('is-open');
+//     menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+//     mobileMenuRef.classList.toggle('is-open');
+//   });
+// })();
